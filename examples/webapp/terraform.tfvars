c594b9df764baa8bbf3fe867d0a237439d2c8d28
Maros Kukan <maros.kukan@me.com>
2021-04-13 15:52:03 +0200

    Add webapp.tf and example tfvars

---
+++ examples/webapp/terraform.tfvars
@@ -0,0 +1,4 @@
+aws_access_key = "<your-access-key-id>"
+aws_secret_key = "<your-secret-access-key-value>"
+key_name = "<your ssh key pair name>"
+private_key_path = "<your ssh private key (.pem) path>"
---
+++ examples/webapp/webapp.tf
@@ -0,0 +1,93 @@
+# Variables
+variable "aws_access_key" {}
+variable "aws_secret_key" {}
+variable "private_key_path" {}
+variable "key_name" {}
+variable "region" {
+    default = "us-east-1"
+}
+
+# Providers
+provider "aws" {
+    access_key = var.aws_access_key
+    secret_key = var.aws_secret_key
+    region = var.region
+}
+
+# Data sources
+data "aws_ami" "aws-linux" {
+    most_recent = true
+    owners = ["amazon"]
+
+    filter {
+        name = "name"
+        values = ["amzn-ami-hvm*"]
+    }
+
+    filter {
+        name = "root-device-type"
+        values = ["ebs"]
+    }
+
+    filter {
+        name = "virtualization-type"
+        values = ["hvm"]
+    }
+}
+
+# Resources
+resource "aws_default_vpc" "default" {
+
+}
+
+resource "aws_security_group" "allow_ssh" {
+    name = "nginx_demo"
+    description = "Allow ports for nginx demo"
+    vpc_id = aws_default_vpc.default.id
+
+    ingress {
+        from_port = 22
+        to_port = 22
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+    ingress {
+        from_port = 80
+        to_port = 80
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+    egress {
+        from_port = 0
+        to_port = 0
+        protocol = -1
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+}
+
+resource "aws_instance" "nginx" {
+    ami = data.aws_ami.aws-linux.id
+    instance_type = "t2.micro"
+    key_name = var.key_name
+    vpc_security_group_ids = [aws_security_group.allow_ssh.id]
+
+    connection {
+        type = "ssh"
+        host = self.public_ip
+        user = "ec2-user"
+        private_key = file(var.private_key_path)
+
+    }
+
+    provisioner "remote-exec" {
+        inline = [
+            "sudo yum install nginx -y",
+            "sudo service nginx start"
+        ]
+    }
+}
+
+# Output
+output "aws_instance_public_dns" {
+    value = aws_instance.nginx.public_dns
+}

efd8937b379586f91b919a64b456f7b3bd861981
Maros Kukan <maros.kukan@me.com>
2021-04-14 05:42:16 +0200

    Add redundancy - ELB, VPC subnet and EC2 instance

--- examples/webapp/webapp.tf
+++ examples/webapp/webapp.tf
@@ -12,6 +12,9 @@ variable "network_address_space" {
 variable "subnet1_address_space" {
     default = "10.1.0.0/24"
 }
+variable "subnet2_address_space" {
+    default = "10.1.1.0/24"
+}
 
 # Providers
 provider "aws" {
@@ -61,6 +64,13 @@ resource "aws_subnet" "subnet1" {
     availability_zone = data.aws_availability_zones.available.names[0]
 }
 
+resource "aws_subnet" "subnet2" {
+    cidr_block = var.subnet2_address_space
+    vpc_id = aws_vpc.vpc.id
+    map_public_ip_on_launch = "true"
+    availability_zone = data.aws_availability_zones.available.names[1]
+}
+
 # Routing
 resource "aws_route_table" "rtb" {
     vpc_id = aws_vpc.vpc.id
@@ -75,7 +85,33 @@ resource "aws_route_table_association" "rta-subnet1" {
     route_table_id = aws_route_table.rtb.id
 }
 
+resource "aws_route_table_association" "rta-subnet2" {
+    subnet_id = aws_subnet.subnet2.id
+    route_table_id = aws_route_table.rtb.id
+}
+
 # Security Groups
+resource "aws_security_group" "elb-sg" {
+    name = "nginx_elb_sg"
+    vpc_id = aws_vpc.vpc.id
+
+    # HTTP access from anywhere
+    ingress {
+        from_port = 80
+        to_port = 80
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    # Outbount Internet access
+    egress {
+        from_port = 0
+        to_port = 0
+        protocol = -1
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+}
+
 resource "aws_security_group" "nginx-sg" {
     name = "nginx-sg"
     description = "Allow ports for nginx demo"
@@ -88,12 +124,12 @@ resource "aws_security_group" "nginx-sg" {
         protocol = "tcp"
         cidr_blocks = ["0.0.0.0/0"]
     }
-    # HTTP access from anywhere
+    # HTTP access from VPC
     ingress {
         from_port = 80
         to_port = 80
         protocol = "tcp"
-        cidr_blocks = ["0.0.0.0/0"]
+        cidr_blocks = [var.network_address_space]
     }
     # Outbount Internet access
     egress {
@@ -101,6 +137,21 @@ resource "aws_security_group" "nginx-sg" {
         to_port = 0
         protocol = -1
         cidr_blocks = ["0.0.0.0/0"]
+    }
+}
+# ELB Instance
+resource "aws_elb" "web" {
+    name = "nginx-elb"
+    
+    subnets = [aws_subnet.subnet1.id, aws_subnet.subnet2.id]
+    security_groups = [aws_security_group.elb-sg.id]
+    instances = [aws_instance.nginx1.id, aws_instance.nginx2.id]
+
+    listener {
+        instance_port = 80
+        instance_protocol = "http"
+        lb_port = 80
+        lb_protocol = "http"
     }
 }
 
@@ -130,7 +181,32 @@ resource "aws_instance" "nginx1" {
     }
 }
 
+resource "aws_instance" "nginx2" {
+    ami = data.aws_ami.aws-linux.id
+    instance_type = "t2.micro"
+    subnet_id = aws_subnet.subnet2.id
+    vpc_security_group_ids = [aws_security_group.nginx-sg.id]
+    key_name = var.key_name
+
+    connection {
+        type = "ssh"
+        host = self.public_ip
+        user = "ec2-user"
+        private_key = file(var.private_key_path)
+
+    }
+
+    provisioner "remote-exec" {
+        inline = [
+            "sudo yum install nginx -y",
+            "sudo service nginx start",
+            "sudo rm /usr/share/nginx/html/index.html",
+            "echo '<html><head><title>Green Team Server</title></head><body style=\"background-color:#77A032\"><p style=\"text-align: center;\"><span style=\"color:#FFFFFF;\"><span style=\"font-size:28px;\">Green Team</span></span></p></body></html>' | sudo tee /usr/share/nginx/html/index.html"
+        ]
+    }
+}
+
 # Output
 output "aws_instance_public_dns" {
-    value = aws_instance.nginx1.public_dns
+    value = aws_elb.web.dns_name
 }
